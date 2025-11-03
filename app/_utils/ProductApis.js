9472fa9215bd86cd42502ca7fba0750fb1266dcb
Mostafa Refat <mustafarefat96@gmail.com>
2025-11-03 21:57:45 +0200

    Update frontend to connect to production backend

--- app/_utils/ProductApis.js
+++ app/_utils/ProductApis.js
@@ -7,7 +7,14 @@ const getProductById = (id) => axiosClient.get(`/products/${id}?populate=*`);
 const getProductsByCategory = (category) =>
   axiosClient.get(`/products?filters[category][$eq]=${category}&populate=*`);
 
-const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`);
+//const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/products`);/
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export const getProducts = async () => {
+  const res = await fetch(`${API_URL}/api/products`);
+  return res.json();
+};
+
 
 
 export default {
